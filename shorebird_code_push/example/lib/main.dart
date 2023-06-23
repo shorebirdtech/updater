9f638cd250178082a27af73cdaab8429a4ce9ad9
Bryan Oltman <bryan@shorebird.dev>
2023-06-23 18:11:58 -0400

    update example to latest released version

--- shorebird_code_push/example/lib/main.dart
+++ shorebird_code_push/example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shorebird Code Push Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Shorebird Code Push'),
