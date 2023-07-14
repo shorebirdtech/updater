e47b7c039be81ac63abfd7291f5427fc45407715
Felix Angelov <felix@shorebird.dev>
2023-07-14 10:28:34 -0500

    cleanup

--- shorebird_code_push/lib/shorebird_code_push_io.dart
+++ shorebird_code_push/lib/shorebird_code_push_io.dart
@@ -20,41 +20,31 @@ class ShorebirdCodePush implements ShorebirdCodePushBase {
 
   late final ShorebirdCodePushBase _delegate;
 
-  /// Whether the Shorebird Engine is available.
   @override
   bool isShorebirdAvailable() => _delegate.isShorebirdAvailable();
 
-  /// Checks whether a new patch is available for download.
-  ///
-  /// Runs in a separate isolate to avoid blocking the UI thread.
   @override
   Future<bool> isNewPatchAvailableForDownload() {
     return _delegate.isNewPatchAvailableForDownload();
   }
 
-  /// The version of the currently-installed patch. `0` if no patch is
-  /// installed (i.e., the app is running the release version).
-  ///
-  /// This will return `null` if Shorebird is not available.
   @override
   Future<int?> currentPatchNumber() => _delegate.currentPatchNumber();
 
-  /// The version of the patch that will be run on the next app launch. If no
-  /// new patch has been downloaded, this will be the same as
-  /// [currentPatchNumber].
   @override
   Future<int?> nextPatchNumber() => _delegate.nextPatchNumber();
 
-  /// Downloads the latest patch, if available.
   @override
-  Future<void> downloadUpdateIfAvailable() =>
-      _delegate.downloadUpdateIfAvailable();
+  Future<void> downloadUpdateIfAvailable() {
+    return _delegate.downloadUpdateIfAvailable();
+  }
 
   /// Whether a new patch has been downloaded and is ready to install.
   ///
   /// If true, the patch number returned by [nextPatchNumber] will be run on the
   /// next app launch.
   @override
-  Future<bool> isNewPatchReadyToInstall() =>
-      _delegate.isNewPatchReadyToInstall();
+  Future<bool> isNewPatchReadyToInstall() {
+    return _delegate.isNewPatchReadyToInstall();
+  }
 }
