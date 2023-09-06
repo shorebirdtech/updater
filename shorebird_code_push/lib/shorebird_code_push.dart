a7afa2e53cad269968cd4d391c6c2e5b66bd635e
Eric Seidel <eric@shorebird.dev>
2023-09-06 09:36:00 -0700

    break: move shorebird_code_push_io and _web into src/
    Users were sometimes importing the _web version thinking it
    was the "web safe" import, rather than shorebird_code_push.dart
    and then being surprised when ShorebirdCodePush did nothing.
    
    Fixes https://github.com/shorebirdtech/shorebird/issues/1209

--- shorebird_code_push/lib/shorebird_code_push.dart
+++ shorebird_code_push/lib/shorebird_code_push.dart
@@ -1,5 +1,5 @@
 /// Get info about your Shorebird code push app
 library shorebird_code_push;
 
-export 'shorebird_code_push_io.dart'
-    if (dart.library.html) 'shorebird_code_push_web.dart';
+export 'src/shorebird_code_push_io.dart'
+    if (dart.library.html) 'src/shorebird_code_push_web.dart';
--- shorebird_code_push/lib/src/shorebird_code_push_web.dart
+++ shorebird_code_push/lib/src/shorebird_code_push_web.dart
@@ -1,6 +1,9 @@
 import 'package:shorebird_code_push/src/shorebird_code_push_base.dart';
 import 'package:shorebird_code_push/src/shorebird_code_push_noop.dart';
 
+/// Applications should not import this file directly, but
+/// import `package:shorebird_code_push/shorebird_code_push.dart` instead.
+
 /// {@template shorebird_code_push}
 /// Get info about your Shorebird code push app.
 /// {@endtemplate}
