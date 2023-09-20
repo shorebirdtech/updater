6a65a46027aed7ee1edca522b527712c2bd2a605
Bryan Oltman <bryan@shorebird.dev>
2023-09-20 10:52:29 -0400

    add trailing comma

--- shorebird_code_push/example/lib/main.dart
+++ shorebird_code_push/example/lib/main.dart
@@ -89,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: CircularProgressIndicator(
               strokeWidth: 2,
             ),
-          )
+          ),
         ],
       ),
     );
