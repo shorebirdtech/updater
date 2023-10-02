8b6e9b7f268946dd7954286b27a3be2e45165527
Bryan Oltman <bryan@shorebird.dev>
2023-10-02 13:30:37 -0400

    cleanup

--- library/src/cache.rs
+++ library/src/cache.rs
@@ -2,13 +2,11 @@ mod disk_io;
 mod patch_manager;
 pub mod updater_state;
 
-use std::path::PathBuf;
-
 pub use updater_state::UpdaterState;
 
 /// The public interface for talking about patches to the Cache.
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct PatchInfo {
-    pub path: PathBuf,
+    pub path: std::path::PathBuf,
     pub number: usize,
 }
