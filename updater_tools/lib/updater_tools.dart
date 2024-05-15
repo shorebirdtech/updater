5707bd8168003a828dd9ed0fe6e3aa6666b15e7c
Bryan Oltman <bryanoltman@gmail.com>
2024-05-15 09:37:37 -0400

    feat: add updater_tools library (#162)
    * feat: add patch_packager library
    
    * remove .github folder
    
    * add patch_packager to ci
    
    * update name to updater_tools
    
    * delete unused file

---
+++ updater_tools/lib/updater_tools.dart
@@ -0,0 +1,4 @@
+/// Tools to create artifacts used by the Updater
+library;
+
+export 'src/updater_tools.dart';
---
+++ updater_tools/test/src/updater_tools_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:updater_tools/updater_tools.dart';
+
+void main() {
+  group('UpdaterTools', () {
+    test('can be instantiated', () {
+      expect(UpdaterTools(), isNotNull);
+    });
+  });
+}
