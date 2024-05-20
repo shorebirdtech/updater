80929401691513bf1ac1d8bb6b9e6498593b72dc
Erick Zanardo <erickzanardoo@gmail.com>
2024-05-20 14:22:42 -0300

    feat: adding CI for updater tools

--- updater_tools/lib/src/extensions/arg_results.dart
+++ updater_tools/lib/src/extensions/arg_results.dart
@@ -4,6 +4,10 @@ import 'package:args/args.dart';
 
 /// Extension methods for validating options provided to [ArgResults].
 extension ArgResultsValidation on ArgResults {
+  /// Returns the value of the option with the given [name] as a [File].
+  ///
+  /// Throws an [ArgumentError] if the option is not present or if the file
+  /// does not exist.
   File asExistingFile(String name) {
     final file = File(this[name] as String);
     if (!file.existsSync()) {
