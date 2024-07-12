eac1158955f3e3906fc766afe7c92c9bee18a191
Bryan Oltman <bryan@shorebird.dev>
2024-07-12 09:23:33 -0400

    docs: add doc comment for asExistingFile

--- updater_tools/lib/src/extensions/arg_results.dart
+++ updater_tools/lib/src/extensions/arg_results.dart
@@ -4,6 +4,8 @@ import 'package:args/args.dart';
 
 /// Extension methods for validating options provided to [ArgResults].
 extension ArgResultsValidation on ArgResults {
+  /// Returns the value of the option named [name] as a [File]. If the file
+  /// does not exist, an [ArgumentError] is thrown.
   File asExistingFile(String name) {
     final file = File(this[name] as String);
     if (!file.existsSync()) {
