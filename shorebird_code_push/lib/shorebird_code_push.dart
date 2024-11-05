25821df380d11a818759b231d21e3fafac33c6e5
Felix Angelov <felix@shorebird.dev>
2024-11-05 11:42:58 -0600

    fix(shorebird_code_push): tighten library exports

--- shorebird_code_push/lib/shorebird_code_push.dart
+++ shorebird_code_push/lib/shorebird_code_push.dart
@@ -1,4 +1,10 @@
 /// Get info about your Shorebird code push app
 library shorebird_code_push;
 
-export 'src/shorebird_updater.dart';
+export 'src/shorebird_updater.dart'
+    show
+        Patch,
+        ShorebirdUpdater,
+        UpdateException,
+        UpdateFailureReason,
+        UpdateStatus;
